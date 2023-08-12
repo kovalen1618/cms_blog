18f10130fac4caac81dec1f473aa8ed277ad83ca
kovalen1618 <kovalen1.618@gmail.com>
2023-08-12 00:56:10 -0500

    Created main skeleton and edited tutorial
    The tutorial is 2 years out of date and so
    there were several things that needed to be
    updated such as the tailwindcss and SaaS
    implementation as well as the app directory

--- app/page.js
+++ app/page.js
@@ -1,11 +1,29 @@
-import Image from 'next/image'
+// Components
+import { PostCard, Categories, PostWidget } from '../components';
+
+// Demo list of posts
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard key={post.title} post={post} />
+          ))}
+        </div>
       </div>
-    </main>
+      <div className="lg:col-span-4 col-span-1">
+        <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+        </div>
+      </div>
+
+    </div>
   )
 }
